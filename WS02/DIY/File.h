52f30fe1f712a2c0ec543747157e471aab73a502
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2023-05-19 08:24:44 -0400

    Delete WS02 directory

--- WS02/DIY/File.h
+++
@@ -1,9 +0,0 @@
-#ifndef SDDS_FILE_H_
-#define SDDS_FILE_H_
-namespace sdds {
-   bool openFile(const char filename[]);
-   void closeFile();
-   int noOfRecords();
-
-}
-#endif // !SDDS_FILE_H_
--- WS02/DIY/Population.h
+++
@@ -1,8 +0,0 @@
-#ifndef SDDS_POPULATION_H_
-#define SDDS_POPULATION_H_
-namespace sdds {
-
-
-
-}
-#endif // SDDS_POPULATION_H_
